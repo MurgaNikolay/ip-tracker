225c49ff4a9f96ffeb89c048e25ddc4ca2381c7c
Nikolay Murga <nikolay.m@murga.kiev.ua>
2015-07-09 17:37:02 +0300

    Fix IP tracking

--- apps/web/controllers/home/index.rb
+++ apps/web/controllers/home/index.rb
@@ -3,15 +3,11 @@ module Web::Controllers::Home
     include Web::Action
 
     def call(params)
-      puts params.inspect
-      puts params.env.inspect
-
       visitor = Visitor.new
       visitor.visited_at = DateTime.now
-      visitor.ipaddress = params.env['CLIENT_IP'] || params.env['REMOTE_ADDR']
+      visitor.ipaddress = params.env['HTTP_CLIENT_IP'] || params.env['REMOTE_ADDR']
       visitor.user_agent = params.env['HTTP_USER_AGENT']
       VisitorRepository.create(visitor)
-
     end
   end
 end
